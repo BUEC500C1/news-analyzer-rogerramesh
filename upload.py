61605b1495f99ac97d87700a8973d9a40145fd4b
roger <rmramesh@bu.edu>
2021-02-17 13:49:43 -0500

    Create upload.py

---
+++ upload.py
@@ -0,0 +1,24 @@
+"""
+User stories
+Create a DB that can store the files that being uploaded.
+Set a max file size threshold that cannot be exceeded
+API should be able to browse through local files on computer
+"""
+# Browse files on local computer
+def browse():
+    """
+    Checks local computer to see directories and what files are available
+    """
+    
+#establish connection to DB
+def connect():
+    """
+    Connects to DB hosted on Server
+    """
+    
+#method to file upload Python script
+def sendtoDB():
+    """
+    retrieves file held on browse() method and sends it directly to a database
+    """
+
