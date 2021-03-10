6308f1f0c1ca56c7cceec321dea8c6433fd28811
roger <rmramesh@bu.edu>
2021-03-10 13:56:09 -0500

    line by line

--- app.py
+++ app.py
@@ -72,6 +72,27 @@ def parse_doc():
     counter=1
     flag=1
 # extract text and do the search
+   
+   for i in range(NumPages):
+        # Creating a page object
+        pageObj = pdfReader.getPage(i)
+        # Printing Page Number
+        print("Page No: ",i)
+        # Extracting text from page
+        # And splitting it into chunks of lines
+        text = pageObj.extractText().split("  ")
+        # Finally the lines are stored into list
+        # For iterating over list a loop is used
+        for i in range(len(text)):
+                # Printing the line
+                # Lines are seprated using "\n"
+                print(text[i],end="\n\n")
+        # For Seprating the Pages
+        print()
+   
+   
+   
+   """
     for i in range(0, NumPages):
         counter = counter+1
         PageObj = object.getPage(i)
@@ -90,7 +111,7 @@ def parse_doc():
         f.write("!!Page " +str(i) +" !!" + Text)
         f.write("\n")
         f.close()
-      
+    """  
     
     #print("Current Directory",retval)
     #file1 = open("data.txt") 
--- test.py
+++ test.py
@@ -12,8 +12,26 @@ NumPages = object.getNumPages()
 #print(NumPages)
 String = "The end"
 
+for i in range(NumPages):
+        # Creating a page object
+        pageObj = object.getPage(i)
+        # Printing Page Number
+        print("Page No: ",i)
+        # Extracting text from page
+        # And splitting it into chunks of lines
+        text = pageObj.extractText().split("  ")
+        # Finally the lines are stored into list
+        # For iterating over list a loop is used
+        for i in range(len(text)):
+                # Printing the line
+                # Lines are seprated using "\n"
+                print(text[i],end="\n\n")
+        # For Seprating the Pages
+        print()
 
 
+
+"""
 # extract text and do the search
 for i in range(0, NumPages):
     PageObj = object.getPage(i)
@@ -29,7 +47,7 @@ for i in range(0, NumPages):
     #print(Text)
     #ResSearch = re.search(String, Text)
     #print(ResSearch)
-
+"""
 
 #print(Text)
 #print("Current Directory",retval)
